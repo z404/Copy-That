8c93624acd16ecb7c054e9377092b35c8a007674
sankalpmukim <sankalpmukim@gmail.com>
2021-07-27 13:08:13 +0530

    ✔️Implement: Show hide textarea

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
 const copyTextToClipboard = (text) => {
-    var textArea = document.createElement("textarea");
-    textArea.value = text;
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
 
-    // Avoid scrolling to bottom
-    textArea.style.top = "0";
-    textArea.style.left = "0";
-    textArea.style.position = "fixed";
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
 
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
 
-    try {
-        document.execCommand("copy");
-    } catch (err) {
-        console.error("Fallback: Oops, unable to copy", err);
-    }
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
 
-    document.body.removeChild(textArea);
+  document.body.removeChild(textArea);
+};
+
+const toggle = () => {
+  const x = document.getElementById("rawtext");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    document.getElementById("copythat").innerText = "Copy that!";
+  } else {
+    render();
+    x.style.display = "none";
+    document.getElementById("copythat").innerText = "Edit";
+  }
 };
 
 const render = () => {
-    //Hide text box, change button text to "edit buttons"
-    const rawText = document.getElementById("rawtext").value.trim();
-    console.log(rawText);
-    const arrayText = rawText.split("\n");
-    // console.log(arrayText);
-    const ul = document.querySelector("ul");
-    ul.innerText = "";
-    for (const element of arrayText) {
-        if (element === "") {
-            continue;
-        }
-        const li = document.createElement("li");
-        const text = document.createElement("span");
-        text.innerText = element;
-        text.setAttribute("class", "text");
-        const button = document.createElement("button");
-        button.addEventListener("click", () => copyTextToClipboard(element));
-        "btn btn-primary clipbutton"
-        .split(" ")
-            .forEach((cls) => button.classList.add(cls));
-        button.innerText = "Copy that!";
-        li.appendChild(text);
-        li.appendChild(button);
-        ul.appendChild(li);
+  //Hide text box, change button text to "edit buttons"
+  const rawText = document.getElementById("rawtext").value.trim();
+  console.log(rawText);
+  const arrayText = rawText.split("\n");
+  // console.log(arrayText);
+  const ul = document.querySelector("ul");
+  ul.innerText = "";
+  for (const element of arrayText) {
+    if (element === "") {
+      continue;
     }
+    const li = document.createElement("li");
+    const text = document.createElement("span");
+    text.innerText = element;
+    text.setAttribute("class", "text");
+    const button = document.createElement("button");
+    button.addEventListener("click", () => copyTextToClipboard(element));
+    "btn btn-primary clipbutton"
+      .split(" ")
+      .forEach((cls) => button.classList.add(cls));
+    button.innerText = "Copy that!";
+    li.appendChild(text);
+    li.appendChild(button);
+    ul.appendChild(li);
+  }
 };
